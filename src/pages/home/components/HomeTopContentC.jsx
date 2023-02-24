de0f87ab74060e851897b2f44dd6b1dfa738c106
abrusdev <abrusdev@gmail.com>
2023-02-24 16:37:43 +0500

    add: ui changes with HomeTopContentC.

--- src/pages/home/components/HomeTopContentC.jsx
+++ src/pages/home/components/HomeTopContentC.jsx
@@ -8,20 +8,40 @@ const useStyles = makeStyles(() => ({
     height: 100,
 
     display: 'flex',
+    justifyContent: 'space-between',
     alignItems: 'center',
 
     background: ({ colors }) => colors.primaryColor,
-    padding: '16px 40px'
+    padding: '16px 60px'
   },
   label: {
     color: ({ colors }) => colors.hintColor,
     fontWeight: 700,
-    fontSize: 16
+    fontSize: 16,
+    opacity: 0.7
   },
   title: {
+    marginTop: 5,
     color: ({ colors }) => colors.contentColor,
     fontWeight: 400,
-    fontSize: 24
+    fontSize: 24,
+    opacity: 0.9
+  },
+  rightContent: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 20
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    background: ({ colors }) => colors.contentColor,
+    borderRadius: 20,
+    padding: 4,
+  },
+  name: {
+    fontSize: 20,
+    color: ({ colors }) => colors.contentColor
   }
 }))
 
@@ -35,8 +55,12 @@ function HomeTopContentC() {
   return (
     <div className={styles.content}>
       <div>
-        <div className={styles.label}>Good Morning</div>
-        <p className={styles.title}>{user.name}</p>
+        <div className={styles.label}>Good Day</div>
+        <p className={styles.title}>Welcome Back</p>
+      </div>
+      <div className={styles.rightContent}>
+        <img className={styles.avatar} src="/images/avatar.png" alt="avatar" />
+        <p className={styles.name}>{user.name}</p>
       </div>
     </div>
   )
